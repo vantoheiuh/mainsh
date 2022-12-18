7d47264baacbc45efb453e9a3cdeb3e0dcb63d7e
vantoheiuh <42094927+vantoheiuh@users.noreply.github.com>
2022-12-18 20:01:07 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -58,6 +58,17 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
+upload_proxy() {
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
+
+}
+
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
@@ -113,5 +124,7 @@ bash /etc/rc.local
 gen_proxy_file_for_user
 rm -rf /root/setup.sh
 rm -rf /root/3proxy-3proxy-0.8.6
+
+upload_proxy
 
 echo "Starting Proxy"
